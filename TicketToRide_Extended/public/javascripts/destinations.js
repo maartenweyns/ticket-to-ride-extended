dc0ec5d6216482d702296f4e854a9ed4fef66c73
Maarten Weyns <maarten.weyns@mawey.be>
2020-04-27 21:40:54 +0200

    Added dot pulsating on route on top

--- TicketToRide_Extended/public/javascripts/destinations.js
+++ TicketToRide_Extended/public/javascripts/destinations.js
@@ -115,12 +115,18 @@ function cycleBetweenRouteCards() {
     for (let i = 0; i < cards.length; i++) {
         if (cards[i].style.display === "block" || cards[i].style.display === "") {
             cards[i].style.display = "none";
+            document.getElementById(cards[i].id.split("-")[0] + "Highlight").classList.remove("pulsatingDots");
+            document.getElementById(cards[i].id.split("-")[1] + "Highlight").classList.remove("pulsatingDots");
             if (i < cards.length - 1) {
                 cards[i + 1].style.display = "block";
                 cardDeal.play();
+                document.getElementById(cards[i + 1].id.split("-")[0] + "Highlight").classList.add("pulsatingDots");
+                document.getElementById(cards[i + 1].id.split("-")[1] + "Highlight").classList.add("pulsatingDots");
             } else {
                 cards[0].style.display = "block";
                 cardDeal.play();
+                document.getElementById(cards[0].id.split("-")[0] + "Highlight").classList.add("pulsatingDots");
+                document.getElementById(cards[0].id.split("-")[1] + "Highlight").classList.add("pulsatingDots");
             }
             break;
         }
